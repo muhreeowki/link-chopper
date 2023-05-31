9f39703507d98ad180584e86a3ca321b1d1080c1
muh <muh@Jemy.local>
2023-05-31 15:11:11 +0300

    Cleaning up some code.

--- client/src/components/form.jsx
+++ client/src/components/form.jsx
@@ -132,6 +132,7 @@ const Form = () => {
       })
       .catch((err) => {
         // Handle Error
+        console.log(err)
       });
   };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { initializeApp } from 'firebase/app'
 import App from "./App.jsx";
-import "./index.css";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAD3QTbDXGJCepRWMdSUCBkrUOj-xk56kA",
